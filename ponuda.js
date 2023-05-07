577d60c2b17561164180ba29d6985dbd61c98b47
TisicPetar <92939307+TisicPetar@users.noreply.github.com>
2023-05-07 13:11:44 +0200

    Add files via upload

---
+++ ponuda.js
@@ -0,0 +1,10 @@
+function otvoriWeb() {
+    window.location.href = "web.html";
+  }
+  function otvoriBusiness() {
+    window.location.href = "binis.html";
+  }
+  function Ponuda() {
+    window.location.href = "ponuda.html";
+  }
+  
